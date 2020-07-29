1b9cf7e86303ffc8d7c4af6234108d6b7f270973
SebBruyere <zbeb@me.com>
2020-07-29 18:48:17 +0200

    Updates

---
+++ precache-manifest.687fcb06ad0cc9e78554f3baed7ce50e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68e3e72d1ef6a2e3971fe4461bb5db0a",
+    "url": "/bodyguard-test/index.html"
+  },
+  {
+    "revision": "fc22398830ba4232f442",
+    "url": "/bodyguard-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fbb8362fddc018cad02e",
+    "url": "/bodyguard-test/static/js/2.a4a4388e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bodyguard-test/static/js/2.a4a4388e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc22398830ba4232f442",
+    "url": "/bodyguard-test/static/js/main.2f5ff87f.chunk.js"
+  },
+  {
+    "revision": "b2fed30d33b34819d973",
+    "url": "/bodyguard-test/static/js/runtime-main.6fa1cbdf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bodyguard-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
